b5005ee52b66c8e2515b404938eccb21b2beea18
vga9764-bit <vga9764@gmail.com>
2026-05-03 21:25:31 +0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'user-app-v2';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        './index.html',
+        './style.css',
+        './script.js',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
